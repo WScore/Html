89dd9641622c2290bc495d6b2258ce60b1bda0c2
asao Kamei <asao@ubuPC.(none)>
2013-02-19 11:34:43 +0900

    added test suites for WScore/Html.

---
+++ tests/WScore/Html/AllHtmlTests.php
@@ -0,0 +1,17 @@
+<?php
+namespace WScore\tests\Html;
+
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
+
+class AllHtmlTests
+{
+    public static function suite()
+    {
+        $suite = new \PHPUnit_Framework_TestSuite( 'all tests for WScore\'s Validation' );
+        $suite->addTestFile( __DIR__ . '/TagsTest.php' );
+        $suite->addTestFile( __DIR__ . '/ElementsTest.php' );
+
+        return $suite;
+    }
+}
